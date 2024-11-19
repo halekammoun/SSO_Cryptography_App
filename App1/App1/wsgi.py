0b9f6b56726955820e090ec190b6000fca099ef3
halekammoun <kammoun.hala@tek-up.de>
2024-11-19 21:15:45 +0100

    webappexample renamed

--- App1/App1/wsgi.py
+++ App1/App1/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webappexample.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "App1.settings")
 
 application = get_wsgi_application()
